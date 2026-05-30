2c36807ccdffd1415e61f9602a7352d17e783642
spLeetzz <nubsplitzz@gmail.com>
2026-05-30 23:54:23 +0530

    lil

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -3,26 +3,23 @@ import { env } from "./env";
 
 type LoggerLevel = "error" | "info" | "debug";
 
-const level: LoggerLevel =
-	env.LOGGER_LEVEL ?? (env.NODE_ENV === "development" ? "debug" : "error");
+const level: LoggerLevel = env.LOGGER_LEVEL ?? (env.NODE_ENV === "development" ? "debug" : "error");
 
 const isDevelopment = env.NODE_ENV === "development";
 
 const format = isDevelopment
-	? winston.format.combine(
-			winston.format.colorize(),
-			winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-			winston.format.printf(({ timestamp, level, message, ...meta }) => {
-				const metaString = Object.keys(meta).length
-					? `\n${JSON.stringify(meta, null, 2)}`
-					: "";
-				return `${timestamp} [${level}]: ${message}${metaString}`;
-			}),
-		)
-	: winston.format.combine(winston.format.timestamp(), winston.format.json());
+  ? winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      winston.format.printf(({ timestamp, level, message, ...meta }) => {
+        const metaString = Object.keys(meta).length ? `\n${JSON.stringify(meta, null, 2)}` : "";
+        return `${timestamp} [${level}]: ${message}${metaString}`;
+      }),
+    )
+  : winston.format.combine(winston.format.timestamp(), winston.format.json());
 
 export const logger = winston.createLogger({
-	level: level,
-	format: format,
-	transports: [new winston.transports.Console()],
+  level: level,
+  format: format,
+  transports: [new winston.transports.Console()],
 });
